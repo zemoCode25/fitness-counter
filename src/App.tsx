3a89ca2d12f424313f69a0d27d36f08692446d4d
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2025-03-02 22:04:20 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <main className="w-dvw">
+        <h1 className="font-semibold">TANGINA MO</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
